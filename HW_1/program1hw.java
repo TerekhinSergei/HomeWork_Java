0b68c175221c59bb16390718625b3fbb4692b0cb
Terekhin <blackrat72@qip.ru>
2023-02-11 10:26:11 +0300

    add 1 task

--- HW_1/program1hw.java
+++ HW_1/program1hw.java
@@ -1,10 +1,26 @@
 /**
- * program
+ Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+  n! (произведение чисел от 1 до n)
  */
-
+import java.util.*;
 public class program1hw {
     public static void main(String[] args) {
-        System.out.println("test text");
+        System.out.print("Введите любое целое число : ");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.println ("Вы ввели число " + num);
+
+        int snum = 1;
+        int fakt = 1;
+        for (int i = 2; i <= num; i++){
+            snum = snum + i;
+            fakt = fakt*i;  
+            }    
+         
+        System.out.println ("Вы ввели число " + num);
+        System.out.println ("n-ое треугольного число = " + snum);
+        System.out.println ("факториал = " + fakt);
     }
+    
 }
 
