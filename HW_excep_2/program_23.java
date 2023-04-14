844234ecfd76e72f4ea8401fc0c61ae9ec15cd22
Terekhin <blackrat72@qip.ru>
2023-04-14 15:27:33 +0300

    add task23

---
+++ HW_excep_2/program_23.java
@@ -0,0 +1,54 @@
+// Задание 3
+// Дан следующий код, исправьте его там, где требуется:
+// public static void main(String[] args) throws Exception {
+//    try {
+//        int a = 90;
+//        int b = 3;
+//        System.out.println(a / b);
+//        printSum(23, 234);
+//        int[] abc = { 1, 2 };
+//        abc[3] = 9;
+//    } catch (Throwable ex) {
+//        System.out.println("Что-то пошло не так...");
+//    } catch (NullPointerException ex) {
+//        System.out.println("Указатель не может указывать на null!");
+//    } catch (IndexOutOfBoundsException ex) {
+//        System.out.println("Массив выходит за пределы своего размера!");
+//    }
+// }
+// public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+//    System.out.println(a + b);
+// }
+///================================================================================
+// в методе main нет необходимости бросать исключение Exception, так как выше нет никаких методов,
+// а так же необходмо изменить очередность обработки исключений в соотвествии
+// с очередностью наследования (иерархией). 
+// в методе printSum нет необходимости бросать исключение FileNotFoundException, 
+// так как этот тип исключения не может возникнуть в представленном коде.
+
+// возможный вариант исправления кода представлен ниже :
+
+public class program_23 {
+    public static void main(String[] args) {
+       
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+
+    }
+    public static void printSum(Integer a, Integer b){
+        System.out.println(a + b);
+    }
+    
+}
